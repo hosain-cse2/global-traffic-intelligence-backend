aca689ad38692435b1e5d0eaeebda255c1862c27
Hosain Ibna Bashar <hosain.cse2@gmail.com>
2026-04-17 04:11:08 +0200

    get all ship without filter

--- src/controllers/aisstream.controller.ts
+++ src/controllers/aisstream.controller.ts
@@ -6,7 +6,7 @@ import {
 } from "../services/aisstream/shipStore.js";
 
 const listShipsController = async (req: Request, res: Response<Ship[]>) => {
-  const ships = shipStore.getAvailableShips();
+  const ships = shipStore.getAll();
   res.status(200).json(ships);
 };
 
